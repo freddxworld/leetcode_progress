1310646eec4d2299a0e11a4fc471b817ce135a2a
freddxworld <98077586+freddxworld@users.noreply.github.com>
2025-09-26 16:59:33 -0700

    [Solved] Arrays - contains_duplicates : only issue i had was with how to set up the set() {HashMap}

--- problems/arrayNhashing/contains_duplicates.py
+++ problems/arrayNhashing/contains_duplicates.py
@@ -1,7 +1,15 @@
-def containsDuplicate(nums):
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
         for num in nums:
             if num in seen:
                 return True
             seen.add(num)
-        return False  
+        return False
+
+
+nums = [1,2,3,1]
+result = Solution().containsDuplicate(nums)
+print(result)
+# O(n) time cause of for, O(n) space cause of hash
